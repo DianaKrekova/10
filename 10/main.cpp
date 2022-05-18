a1309c0ef821ada3a27dddfc89824b16e77b78ea
DianaKrekova <68234843+DianaKrekova@users.noreply.github.com>
2022-05-18 14:05:27 +0300

    Update main.cpp

--- 10/main.cpp
+++ 10/main.cpp
@@ -2,9 +2,6 @@
 
 int main() {
 	setlocale(LC_ALL, "rus");
-	cout << "Creating an object of the abstract Human class: \n";
-	Human H("hi", "im", "human", 10);
-	H.print();
 	cout << "Creating an object of the Student inheritor class: \n";
 	Student S("hi", "im", "Student", 15, 1);
 	S.print();
